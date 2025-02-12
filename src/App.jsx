d99bba184a91d30c1a41893d4be43bd083167f30
Lybrook <omoshlybrook@gmail.com>
2025-02-12 17:49:52 +0300

    add App component to render FiverrPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FiverrPage from './components/FiverrPage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <FiverrPage />
+    </div>
+  );
+}
+
+export default App;
